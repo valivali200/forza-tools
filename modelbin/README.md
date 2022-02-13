db6c0f2fc989e220cca559c2c32352d0b037202e
valivali200 <valiv4491@gmail.com>
2022-02-13 21:09:00 +0200

    Added grouping support for modelbin tool

--- modelbin/README.md
+++ modelbin/README.md
@@ -2,14 +2,18 @@
 This tool converts the .modelbin format to Wavefront obj.  
 >Usage: `python modelbin.py <input file>`
 >Example: `python modelbin.py hood.modelbin`
->*Outputs to **out.obj***
+>*Outputs to **out.obj***  
 
+Make sure to split model by group when importing.  
+Example for Blender:  
+
+![](./split-by-groups.png)
 
 ### **TODO**:
 - Vertex positions ✓
 - Face indices ✓
 - Correct scaling
-- Grouping
+- Grouping ✓
 - UVs
 - Materials
 - etc.
--- modelbin/modelbin.py
+++ modelbin/modelbin.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import struct
 
 def getUint32(offset):
     return int.from_bytes(model[offset:offset+4], 'little')
@@ -57,6 +58,8 @@ for i in range(entries_count):
 
 IndBIdx = []
 VerBIdx = []
+MeshIdx = []
+MatIIdx = []
 
 # parse entry indices into lists
 for i in range(entries_count):
@@ -64,15 +67,68 @@ for i in range(entries_count):
         IndBIdx.append(i)
     if entries[i][0] == b'BreV':
         VerBIdx.append(i)
+    if entries[i][0] == b'hseM':
+        MeshIdx.append(i)
+    if entries[i][0] == b'ItaM':
+        MatIIdx.append(i)
+
+print('Materials:', len(MatIIdx))
+
+# read bytes from entry headers
+entry_headers_data = []
+for i in range(entries_count):
+    if entries[i] == entries[-1]:
+        header_start = entries[i][1]
+        header_size = entries[0][2] - header_start
+    else:
+        header_start = entries[i][1]
+        header_size = entries[i+1][1] - entries[i][1]
+    if header_size == 0:
+        entry_headers_data.append([])
+    else:
+        entry_headers_data.append(model[header_start:header_start+header_size])
+
+# parse entry headers
+entry_headers = []
+for i in entry_headers_data:
+    if i == []:
+        entry_headers.append([])
+        continue
+    bbox = []
+    name_length = int.from_bytes(i[14:16], 'little') - 8
+    name = i[16:16+name_length]
+    if i[8:12] == b'xoBB':
+        bbox = struct.unpack('6f', i[-24:])
+    entry_headers.append([name, bbox])    
+
+# get groups
+groups = []
+for i in MeshIdx:
+    name = entry_headers[i][0]
+    if name == b'Shadow':
+        continue
+    group_size = getUint32(entries[i][2]+43)
+    groups.append([name, group_size])
 
 # parse vertex positions from first VerB
 vpos_offset = entries[VerBIdx[0]][2]
 vpos_data = getVertexPositions(vpos_offset)
 for i in vpos_data: # write to obj
     obj.write('v ' + str(i[0]) + ' ' + str(i[1]) + ' ' + str(i[2]) + '\n')
+print('Vertices:', len(vpos_data))
 
 # parse face indices
 fi_offset = entries[IndBIdx[0]][2]
 fi_data = getFaceIndices(fi_offset)
+fi_count = 0
+obj.write('g ' + str(groups[0][0])[2:-1] + '\n')
+group_count = 1
+next_group_start = groups[0][1]
 for i in fi_data:
+    if fi_count == next_group_start:
+        obj.write('g ' + str(groups[group_count][0])[2:-1] + '\n')
+        next_group_start += groups[group_count][1]
+        group_count += 1
     obj.write('f ' + str(i[0]) + ' ' + str(i[1]) + ' ' + str(i[2]) + '\n')
+    fi_count += 1
+print('Face indices:', len(fi_data))
